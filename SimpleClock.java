6b2d1f9d8cfd9a9062fd57eceda63bb6ac82561a
ninachen22 <116048270+ninachen22@users.noreply.github.com>
2022-12-11 23:13:42 -0500

    working but timezone change require 2 clicks to get date change

--- SimpleClock.java
+++ SimpleClock.java
@@ -2,73 +2,110 @@
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
+import java.awt.font.GlyphMetrics;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.TimeZone;
 
 
-public class SimpleClock extends JFrame {
-    
-        Calendar calendar;
-        SimpleDateFormat timeFormat;
-        SimpleDateFormat dayFormat;
-        SimpleDateFormat dateFormat;
-    
-        JLabel timeLabel;
-        JLabel dayLabel;
-        JLabel dateLabel;
-        String time;
-        String day;
-        String date;
+public class SimpleClock extends JFrame implements ItemListener {
 
-        SimpleClock() {
-            this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-            this.setTitle("Digital Clock");
-            this.setLayout(new FlowLayout());
-            this.setSize(350, 220);
-            this.setResizable(false);
-    
-            timeFormat = new SimpleDateFormat("hh:mm:ss a");
-            dayFormat=new SimpleDateFormat("EEEE");
-            dateFormat=new SimpleDateFormat("dd MMMMM, yyyy");
-            timeLabel = new JLabel();
-            timeLabel.setFont(new Font("SANS_SERIF", Font.PLAIN, 59));
-            timeLabel.setBackground(Color.BLACK);
-            timeLabel.setForeground(Color.WHITE);
-            timeLabel.setOpaque(true);
-            dayLabel=new JLabel();
-            dayLabel.setFont(new Font("Ink Free",Font.BOLD,34));
-    
-            dateLabel=new JLabel();
-            dateLabel.setFont(new Font("Ink Free",Font.BOLD,30));
-    
-    
-            this.add(timeLabel);
-            this.add(dayLabel);
-            this.add(dateLabel);
-            this.setVisible(true);
-    
-            setTimer();
-        }
-    
-        public void setTimer() {
-            while (true) {
-                time = timeFormat.format(Calendar.getInstance().getTime());
-                timeLabel.setText(time);
-    
-                day = dayFormat.format(Calendar.getInstance().getTime());
-                dayLabel.setText(day);
-    
-                date = dateFormat.format(Calendar.getInstance().getTime());
-                dateLabel.setText(date);
-    
-                try {
-                    Thread.sleep(1000);
-                } catch (Exception e) {
-                    e.getStackTrace();
-                }
+    Calendar calendar = Calendar.getInstance();
+    SimpleDateFormat timeFormat;
+    SimpleDateFormat dayFormat;
+    SimpleDateFormat dateFormat;
+
+    JLabel timeLabel;
+    JLabel dayLabel;
+    JLabel dateLabel;
+    String time;
+    String day;
+    String date;
+    JToggleButton timezone;
+    JToggleButton militaryTime;
+
+    SimpleClock() {
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setTitle("Digital Clock");
+        this.setLayout(new FlowLayout());
+        this.setSize(350, 300);
+        this.setResizable(false);
+
+        timeFormat = new SimpleDateFormat("hh:mm:ss a");
+        dayFormat = new SimpleDateFormat("EEEE");
+        dateFormat = new SimpleDateFormat("dd MMMMM, yyyy");
+        timeLabel = new JLabel();
+        timeLabel.setFont(new Font("SANS_SERIF", Font.PLAIN, 45));
+        timeLabel.setBackground(Color.BLACK);
+        timeLabel.setForeground(Color.WHITE);
+        timeLabel.setOpaque(true);
+        dayLabel = new JLabel();
+        dayLabel.setFont(new Font("Ink Free", Font.BOLD, 34));
+
+        dateLabel = new JLabel();
+        dateLabel.setFont(new Font("Ink Free", Font.BOLD, 30));
+
+        calendar.getTimeZone();
+        timezone = new JToggleButton("GMT");
+        timezone.addItemListener(this);
+        militaryTime = new JToggleButton("24hr");
+        militaryTime.addItemListener(this);
+
+        this.add(timeLabel);
+        this.add(dayLabel);
+        this.add(dateLabel);
+        this.setVisible(true);
+        this.add(timezone);
+        this.add(militaryTime);
+
+        setTimer();
+    }
+
+    public void setTimer() {
+        while (true) {
+            time = timeFormat.format(Calendar.getInstance().getTime());
+            timeLabel.setText(time);
+
+            day = dayFormat.format(Calendar.getInstance().getTime());
+            dayLabel.setText(day);
+
+            date = dateFormat.format(Calendar.getInstance().getTime());
+            dateLabel.setText(date);
+
+            try {
+                Thread.sleep(1000);
+            } catch (Exception e) {
+                e.getStackTrace();
             }
         }
-        public static void main(String[] args) {
-            new SimpleClock();
-        }
     }
+
+    public static void main(String[] args) {
+        new SimpleClock();
+    }
+
+    @Override
+    public void itemStateChanged(ItemEvent e) {
+        if (timezone.isSelected()) {
+            timezone.setText("EST");
+            dayFormat = new SimpleDateFormat("EEEE");
+            dateFormat = new SimpleDateFormat("dd MMMMM, yyyy");
+            TimeZone.setDefault(TimeZone.getTimeZone("EST"));
+        } else {
+            timezone.setText("GMT");
+            dayFormat = new SimpleDateFormat("EEEE");
+            dateFormat = new SimpleDateFormat("dd MMMMM, yyyy");
+            TimeZone.setDefault(TimeZone.getTimeZone("GMT"));
+        }
+        if (militaryTime.isSelected()) {
+            militaryTime.setText("24hr");
+            timeFormat = new SimpleDateFormat("HH:mm:ss a");
+        } else {
+            militaryTime.setText("12hr");
+            timeFormat = new SimpleDateFormat("hh:mm:ss a");
+        }
+
+    }
+}
